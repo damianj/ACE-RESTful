427c011291b4267a1a6ff6d12abd53b770d844fa
Damian Jimenez <jimenez.dmn@gmail.com>
2017-04-25 21:19:17 -0500

    Finished implementing the CartesianMove endpoint
    Endpoint runs asynchronously, but threads have to wait until each previous call finishes to avoid conflict with issuing too many commands to the robot or having commands overwrite one another.

--- CustomACEApi/CustomACEAPI.cs
+++ CustomACEApi/CustomACEAPI.cs
@@ -9,24 +9,34 @@ using Nancy.Hosting.Self;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CustomACEAPI
 {
     public class APIServer
     {
+        private static SemaphoreSlim _SEMAPHORE= new SemaphoreSlim(1, 1);
+        private const string REMOTING_NAME = "ace";
+        private const string REMOTING_HOST = "localhost";
+        private const int CALLBACK_PORT = 43431;
+        private const int REMOTING_PORT = 43434;
+
         private const string _url = "http://localhost";
         private const int _port = 12345;
         private static NancyHost _nancy;
-        private const string RemotingName = "ace";
-        private const int CallbackPort = 43431;
-        private const int RemotingPort = 43434;
+        private static string _status = "Offline";
         private static IAceServer ace;
+        private static IAdeptRobot robot;
 
         public APIServer()
         {
             var configuration = new HostConfiguration()
             {
-                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+                UrlReservations = new UrlReservations()
+                {
+                    CreateAutomatically = true
+                }
             };
 
             _nancy = new NancyHost(configuration, new Uri($"{_url}:{_port}/"));
@@ -34,10 +44,42 @@ namespace CustomACEAPI
 
         private void Start()
         {
+            ConsoleKeyInfo cki;
             _nancy.Start();
-            Console.WriteLine($"Started listennig on port {_port}");
-            Console.ReadKey();
+            _status = $"Listennig on port {_port}";
+
+            Console.WriteLine(_status);
+            Console.WriteLine("Press ESC or Q/q to exit the application.");
+            do
+            {
+                cki = Console.ReadKey();
+            }
+            while(cki.Key != ConsoleKey.Escape && cki.Key != ConsoleKey.Q);
+
             _nancy.Stop();
+        }
+
+        private class AdeptAce
+        {
+            public void ConnectToServer(String controllerPath="/SmartController 9/SmartController 9", String robotPath= "/SmartController 9/R1 Viper650")
+            {
+
+                RemotingUtil.InitializeRemotingSubsystem(true, CALLBACK_PORT);
+                ace = (IAceServer)RemotingUtil.GetRemoteServerObject(typeof(IAceServer), REMOTING_NAME, REMOTING_HOST, REMOTING_PORT);
+
+                IAdeptController controller = ace.Root[controllerPath] as IAdeptController;
+                controller.Enabled = true;
+                controller.HighPower = true;
+                controller.Calibrate();
+                if (!controller.IsEVPlus)
+                {
+                    controller.DryRun = true;
+                    controller.DryRun = false;
+                }
+                AdeptControllerUtil.AddAllRobots(ace, controller, ace.Root);
+
+                robot = ace.Root["/SmartController 9/R1 Viper650"] as IAdeptRobot;
+            }
         }
 
         public class CartesianMoveAPI : NancyModule
@@ -50,125 +92,116 @@ namespace CustomACEAPI
                     {
                         StatusCode = HttpStatusCode.BadRequest,
                         ContentType = "application/json",
-                        ReasonPhrase = "GET requests not supported by this API endpoint. Please send a POST request with the appropriate payload.",
+                        ReasonPhrase = "GET requests not supported, use a POST request instead.",
                         Headers = new Dictionary<string, string>()
                         {
-                            { "Content-Type", "application/json" },
-                            { "X-Custom-Header", "" }
+                            {
+                                "Content-Type", "application/json"
+                            }
                         },
                     };
                 };
 
-                Post["/CartesianMove"] = _ =>
+                Post["/CartesianMove", runAsync: true] = async (_, token) =>
                 {
+                    await _SEMAPHORE.WaitAsync();
                     try
                     {
-                        var id = this.Request.Body;
-                        var length = this.Request.Body.Length;
-                        var data = new byte[length];
-
-                        id.Read(data, 0, (int)length);
-                        var body = Encoding.Default.GetString(data);
-                        var request = JsonConvert.DeserializeObject<CartesianMove>(body);
-
-                        Console.WriteLine($"{request.Name}, {request.Accel}, {request.Decel}, {request.Speed}, {request.StraightMotion}, {request.MotionEnd}, {request.SCurveProfile}");
-
-                        return 200;
-                    }
-                    catch(Exception e)
-                    {
-                        string jsonString = $"{{ status: \"failure\", error: \"{e.Message}\" }}";
-                        byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
-
-                        return new Response()
+                        try
                         {
-                            StatusCode = HttpStatusCode.BadRequest,
-                            ContentType = "application/json",
-                            ReasonPhrase = "Unable to successfully interpret the request.",
-                            Headers = new Dictionary<string, string>()
+                            var id = this.Request.Body;
+                            var length = this.Request.Body.Length;
+                            var data = new byte[length];
+
+                            id.Read(data, 0, (int)length);
+                            var body = Encoding.Default.GetString(data);
+                            var command = JsonConvert.DeserializeObject<CartesianMoveCommand>(body);
+
+                            Console.WriteLine($"{command.Name}, {command.Accel}, {command.Decel}, {command.Speed}, {command.StraightMotion}, {command.MotionEnd}, {command.SCurveProfile}");
+
+                            await Task.Run(() => { command.Execute(); });
+
+                            return HttpStatusCode.OK;
+                        }
+                        catch(Exception e)
+                        {
+                            string jsonString = $"{{ status: \"failure\", error: \"{e.Message}\" }}";
+                            byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
+
+                            Console.WriteLine(e.Message);
+
+                            return new Response()
                             {
-                                { "Content-Type", "application/json" },
-                                { "X-Custom-Header", "Please check the error message below, and make sure the JSON payload is properly formatted." }
-                            },
-                            Contents = c => c.Write(jsonBytes, 0, jsonBytes.Length)
-                        };
+                                StatusCode = HttpStatusCode.BadRequest,
+                                ContentType = "application/json",
+                                ReasonPhrase = "Unable to successfully interpret the request.",
+                                Headers = new Dictionary<string, string>()
+                                {
+                                    {
+                                        "Content-Type", "application/json"
+                                    },
+                                    {
+                                        "X-Custom-Header", "Please check the error message below, make sure the JSON payload is properly formatted, and that the robot is initialized properly."
+                                    }
+                                },
+                                Contents = c => c.Write(jsonBytes, 0, jsonBytes.Length)
+                            };
+                        }
+                    }
+                    finally
+                    {
+                        _SEMAPHORE.Release(1);
                     }
                 };
             }
         }
 
-        private class CartesianMove
+        private class CartesianMoveCommand
         {
             public string Name = "Cartesian Move";
             public int Accel { get; set; }
             public int Decel { get; set; }
             public int Speed { get; set; }
-            public Boolean StraightMotion { get; set; }
-            public String MotionEnd { get; set; }
+            public bool StraightMotion { get; set; }
+            public string MotionEnd { get; set; }
             public int SCurveProfile { get; set; }
-        }
 
-        static void Main(string[] args)
-        {
-            var api_server = new APIServer();
-            api_server.Start();
-            /*
-            // Initialize remoting infrastructure
-            RemotingUtil.InitializeRemotingSubsystem(true, CallbackPort);
-
-            // Connect to ACE.
-            ace = (IAceServer) RemotingUtil.GetRemoteServerObject(typeof(IAceServer), RemotingName, "localhost", RemotingPort);
-
-            Console.WriteLine(ace.ToString());
-            foreach(IAdeptController cntrlr in ace.Root.Filter(new ObjectTypeFilter(typeof(IAdeptController)), true))
+            public void Execute()
             {
-                Console.WriteLine("Found controller '{0}'", cntrlr);
-                cntrlr.HighPower = true;
-                cntrlr.Calibrate();
-            }
+                Thread.Sleep(3000);
+                // Read various properties of the robot
+                Console.WriteLine("Number of joints = " + robot.JointCount);
 
-            IAdeptController controller = ace.Root["/SmartController 9/SmartController 9"] as IAdeptController;
-            controller.Enabled = true;
-            AdeptControllerUtil.AddAllRobots(ace, controller, ace.Root);
+                double[] jointPositions = robot.JointPosition;
+                for (int i = 0; i < robot.JointCount; i++)
+                {
+                    Console.WriteLine("Joint " + i + " " + jointPositions);
+                }
 
-            IAdeptRobot robot = ace.Root["/SmartController 9/R1 Viper650"] as IAdeptRobot;
+                // Transform the current joint position to a world location
+                try
+                {
+                    Transform3D loc = robot.JointToWorld(jointPositions);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
 
-            // Read various properties of the robot
-            Console.WriteLine("Number of joints = " + robot.JointCount);
+                // Check if the current location is inrange           
+                Transform3D currentPosition = robot.WorldLocationWithTool;
+                int inRange = robot.InRange(currentPosition);
+                Console.WriteLine(currentPosition + " inrange check = " + inRange);
 
-            double[] jointPositions = robot.JointPosition;
-            for (int i = 0; i < robot.JointCount; i++)
-            {
-                Console.WriteLine("Joint " + i + " " + jointPositions);
-            }
+                // Get the current robot configuration
+                IMoveConfiguration moveConfig = robot.GetMoveConfiguration(jointPositions);
 
-            // Transform the current joint position to a world location
-            try
-            {
-                Transform3D loc = robot.JointToWorld(jointPositions);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-            // Check if the current location is inrange           
-            Transform3D currentPosition = robot.WorldLocationWithTool;
-            int inRange = robot.InRange(currentPosition);
-            Console.WriteLine(currentPosition + " inrange check = " + inRange);
-
-            // Get the current robot configuration
-            IMoveConfiguration moveConfig = robot.GetMoveConfiguration(jointPositions);
-
-            // Create a motion object and command the robot to move
-            CartesianMove cartesianMove = ace.CreateObject(typeof(CartesianMove)) as CartesianMove;
-            cartesianMove.MoveConfiguration = moveConfig;
-            Transform3D t1 = new Transform3D(10, 10, 0);
-            t1 = -t1;
-            cartesianMove.WorldLocation = currentPosition * t1;
-
-            try
-            {
+                // Create a motion object and command the robot to move
+                CartesianMove cartesianMove = ace.CreateObject(typeof(CartesianMove)) as CartesianMove;
+                cartesianMove.MoveConfiguration = moveConfig;
+                Transform3D t1 = new Transform3D(10, 10, 0);
+                t1 = -t1;
+                cartesianMove.WorldLocation = currentPosition * t1;
 
                 // Issue the move and wait until it is done
                 robot.Move(cartesianMove);
@@ -176,13 +209,13 @@ namespace CustomACEAPI
 
                 // Force the robot to issue a DETACH
                 robot.AutomaticControlActive = false;
+            }
+        }
 
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            */
+        static void Main(string[] args)
+        {
+            var api_server = new APIServer();
+            api_server.Start();
         }
     }
 }
