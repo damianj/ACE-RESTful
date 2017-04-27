b1037bbe190a961e7db630096fd5be7c348ea01d
Damian Jimenez <jimenez.dmn@gmail.com>
2017-04-27 11:56:17 -0500

    Updated documentation
    Updated the documentation to show the XML comments in the code and filled in missing documentation items.

--- CustomACEApi/MainWindow.xaml.cs
+++ CustomACEApi/MainWindow.xaml.cs
@@ -15,9 +15,17 @@ using Newtonsoft.Json;
 using System.Threading;
 using System.IO;
 
-/// <summary>The following project implements a C# API for the Adept ACE server, it allows robots to be controlled via local server that is run on NancyFX (a light-weight web framework) and allows Adept robots to be easily controlled via other methods (e.g., LabView, Python, or anything that can issue HTTP POST/GET requests).</summary>
+
 namespace CustomACEAPI
 {
+    /// <summary>
+    /// The <see cref="CustomACEAPI"/> project implements a C# RESTful API for the Adept ACE server. It allows robots to be controlled via a local HTTP server that is run on NancyFX (a light-weight web framework), and allows Adept robots to be easily controlled via other mediums (e.g., LabView, Python, or anything that can issue HTTP POST/GET requests).
+    /// </summary>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc
+    {
+    }
+
     /// <summary>Class that handles creating and maintaining an instance of the GUI as well as setting up the application for use.</summary>
     public partial class MainWindow : Window
     {
@@ -29,7 +37,7 @@ namespace CustomACEAPI
 
         /// <summary>Initiates the GUI and preps everything needed to successfully start the NancyFX server and connect to the Adept ACE server </summary>
         /// <author>Damian Jimenez</author>
-        /// <returns>Noting</returns>
+        /// <returns><c>void</c></returns>
         public MainWindow()
         {
             adept_ace = new AdeptAce();
@@ -37,6 +45,7 @@ namespace CustomACEAPI
             OutputText.Text = "";
             Console.SetOut(new MultiTextWriter(new TextBoxWriter(OutputText), Console.Out));
 
+            // Create a URL reservation to allow the NancyFX server to start (i.e., prevent AutomaticUrlReservationCreationFailureException)
             var configuration = new HostConfiguration()
             {
                 UrlReservations = new UrlReservations()
@@ -45,6 +54,7 @@ namespace CustomACEAPI
                 }
             };
 
+            // Prep the NancyFX server for use later on
             _nancy = new NancyHost(configuration, new Uri($"{_url}:{_port}/"));
             Console.WriteLine("App started successfully...\n---------------------------");
         }
@@ -53,14 +63,16 @@ namespace CustomACEAPI
         /// <author>Damian Jimenez</author>
         /// <param name="sender">Object that triggered the call to this method</param>
         /// <param name="e">Event information of the routed event</param>
-        /// <returns>Noting</returns>
+        /// <returns><c>void</c></returns>
         void Start_APP(object sender, RoutedEventArgs e)
         {
+            // If we are already connected to the ACE server don't attempt to re-connect
             if(!adept_ace.Connected)
             {
                 adept_ace.ConnectToServer();
             }
             
+            // Start the HTTP server on localhost:9001
             _nancy.Start();
             Console.WriteLine($"Listening on {_url}:{_port}/\n");
         }
@@ -69,9 +81,10 @@ namespace CustomACEAPI
         /// <author>Damian Jimenez</author>
         /// <param name="sender">Object that triggered the call to this method</param>
         /// <param name="e">Event information of the routed event</param>
-        /// <returns>Noting</returns>
+        /// <returns><c>void</c></returns>
         void Stop_APP(object sender, RoutedEventArgs e)
         {
+            // Stop the HTTP Server
             _nancy.Stop();
             Console.WriteLine("Successfuly stopped the HTTP server.\n------------------------------------\n");
         }
@@ -83,12 +96,12 @@ namespace CustomACEAPI
         {
             /// <summary>API endpoint for the Cartesian Move command</summary>
             /// <author>Damian Jimenez</author>
-            /// <returns>Nothing</returns>
+            /// <returns><c>void</c></returns>
             public CartesianMoveAPI()
             {
                 /// <summary>GET request handler</summary>
                 /// <author>Damian Jimenez</author>
-                /// <returns>Response object specifying to the user that GET requests are not supported</returns>
+                /// <returns><c>Response</c> object specifying to the user that GET requests are not supported</returns>
                 Get["/api/move/cartesian"] = _ =>
                 {
                     return new Response()
@@ -107,7 +120,7 @@ namespace CustomACEAPI
 
                 /// <summary>POST request handler. Takes the JSON payload and attempts to parse the commands and execute a CartesianMove using the Ace.Adept.Server.Motion</summary>
                 /// <author>Damian Jimenez</author>
-                /// <returns>HttpStatusCode.OK or a Response object detailing what went wrong</returns>
+                /// <returns><c>HttpStatusCode.OK</c> or a Response object detailing what went wrong</returns>
                 Post["/api/move/cartesian"] = _ =>
                 {
                     try
@@ -158,6 +171,9 @@ namespace CustomACEAPI
         /// <author>Damian Jimenez</author>
         public class CartesianMoveCommand
         {
+            /// <summary>
+            /// Mapping of all the possible values for <c>MotionEnd</c>, to be easily accessed with a <c>String</c> parsed from the JSON file in the HTTP POST request
+            /// </summary>
             private static Dictionary<string, MotionEnd> MOTION_END = new Dictionary<string, MotionEnd>()
             {
                 { "Blend", Ace.Adept.Server.Motion.MotionEnd.Blend },
@@ -166,25 +182,64 @@ namespace CustomACEAPI
                 { "SettleFine", Ace.Adept.Server.Motion.MotionEnd.SettleFine },
             };
 
+            /// <summary>
+            /// The name of this type of motion, derived from the ACE command name.
+            /// </summary>
             public string Name = "Cartesian Move";
+            /// <summary>
+            /// Maximum acceleration of the robot when it moves.
+            /// </summary>
             public int Accel { get; set; }
+            /// <summary>
+            /// Maximum deceleration of the robot when it moves.
+            /// </summary>
             public int Decel { get; set; }
+            /// <summary>
+            /// Maximum speed of the robot when it moves.
+            /// </summary>
             public int Speed { get; set; }
-            public bool StraightMotion { get; set; }    // Get or set a flag indicating whether the motion should be straight-line. If false, motion will be joint-interpolated. 
-            public string MotionEnd { get; set; }       // One of: Blend, NoNull, SettleCoarse, or SettleFine
-            public int SCurveProfile { get; set; }      // Get or set the S-curve profile number, from 0 (for trapezoidal) to 8, or -1 to leave unchanged.
+            /// <summary>
+            /// Determines whether the robot's motion should be straight or not (i.e., true: straight-line, false: motion will be joint-interpolated)
+            /// </summary>
+            public bool StraightMotion { get; set; }
+            /// <summary>
+            /// Determines how the motion should finish (i.e., Blend, NoNull, SettleCoarse, or SettleFine)
+            /// </summary>
+            public string MotionEnd { get; set; }
+            /// <summary>
+            /// Specifies the S-curve trajectory profile, from 0 (for trapezoidal) to 8, or -1 to leave unchanged.
+            /// </summary>
+            public int SCurveProfile { get; set; }
+            /// <summary>
+            /// Specifies the X coordinate of the motion
+            /// </summary>
             public double X { get; set; }
+            /// <summary>
+            /// Specifies the Y coordinate of the motion
+            /// </summary>
             public double Y { get; set; }
+            /// <summary>
+            /// Specifies the Z coordinate of the motion
+            /// </summary>
             public double Z { get; set; }
+            /// <summary>
+            /// Specifies the Yaw coordinate of the motion
+            /// </summary>
             public double Yaw { get; set; }
+            /// <summary>
+            /// Specifies the Pitch coordinate of the motion
+            /// </summary>
             public double Pitch { get; set; }
+            /// <summary>
+            /// Specifies the Roll coordinate of the motion
+            /// </summary>
             public double Roll { get; set; }
 
             /// <summary>Method that executes a command for an instance of the CartesianMoveCommand class</summary>
             /// <author>Damian Jimenez</author>
             /// <param name="robot">The robot that is to be controlled via the API call</param>
             /// <param name="cartesianMove">Cartesian move object to handle calculating the motion of the robot</param>
-            /// <returns>Nothing</returns>
+            /// <returns><c>void</c></returns>
             public void Execute(IAdeptRobot robot, CartesianMove cartesianMove)
             {
                 _SEMAPHORE.WaitOne();
@@ -244,71 +299,110 @@ namespace CustomACEAPI
         }
     }
 
-    /// <summary>Class to assist in re-routing STDOUT to the GUI text-box that is displayed</summary>
+    /// <summary>Class to assist in re-routing <c>stdout</c> to the GUI text-box that is displayed</summary>
     /// <author>Damian Jimenez -- [http://stackoverflow.com/a/18727100]</author>
     public class TextBoxWriter : TextWriter
     {
         private TextBox textbox;
 
+        /// <summary>Constructor method that instantiates the <c>TextBox</c> instance to the appropriate element in the GUI</summary>
+        /// <param name="textbox"><c>TextBox</c> instance that is to be used to write to.</param>
+        /// <author>Damian Jimenez</author>
         public TextBoxWriter(TextBox textbox)
         {
             this.textbox = textbox;
         }
 
+        /// <summary>Override of the <c>Write</c> method</summary>
+        /// <param name="value"><c>char</c> to be written</param>
+        /// <returns><c>void</c></returns>
+        /// <author>Damian Jimenez</author>
         public override void Write(char value)
         {
             textbox.Text += value;
         }
 
+        /// <summary>Override of the <c>Write</c> method, writes to each <c>TextWriter</c> in the list of <c>TextWriter</c>s</summary>
+        /// <param name="value"><c>string</c> to be written</param>
+        /// <returns><c>void</c></returns>
+        /// <author>Damian Jimenez</author>
         public override void Write(string value)
         {
             textbox.Text += value;
         }
 
+        /// <summary>Override of the <c>Encoding</c> method, returns the <c>Encoding</c> of the <c>TextBox</c></summary>
+        /// <returns><c>Encoding.ASCII</c></returns>
+        /// <author>Damian Jimenez</author>
         public override Encoding Encoding
         {
             get { return Encoding.ASCII; }
         }
     }
 
-    /// <summary>Class to assist in re-routing STDOUT to multiple outputs. In our case to re-route it to the text-box that is displayed and also to the console as well.</summary>
+    /// <summary>Class to assist in re-routing <c>stdout</c> to multiple outputs. In our case to re-route it to the text-box that is displayed and also to the console as well.</summary>
     /// <author>Damian Jimenez -- [http://stackoverflow.com/a/18727100]</author>
     public class MultiTextWriter : TextWriter
     {
         private IEnumerable<TextWriter> writers;
+
+        /// <summary>Overload of the constructor method that takes an <c>IEnumerable</c> of type <c>TextWriter</c> to be converted to a list.</summary>
+        /// <param name="writers"><c>IEnumerable</c> of <c>TextWriter</c> objects that are to be converted to a list, to be used to route <c>stdout</c> to.</param>
+        /// <author>Damian Jimenez</author>
         public MultiTextWriter(IEnumerable<TextWriter> writers)
         {
             this.writers = writers.ToList();
         }
+
+        /// <summary>Overload of the constructor method that takes a list of <c>TextWriter</c> objects.</summary>
+        /// <param name="writers">List of <c>TextWriter</c> objects that are to be used to route <c>stdout</c> to.</param>
+        /// <author>Damian Jimenez</author>
         public MultiTextWriter(params TextWriter[] writers)
         {
             this.writers = writers;
         }
 
+        /// <summary>Override of the <c>Write</c> method</summary>
+        /// <param name="value"><c>char</c> to be written</param>
+        /// <returns><c>void</c></returns>
+        /// <author>Damian Jimenez</author>
         public override void Write(char value)
         {
             foreach (var writer in writers)
                 writer.Write(value);
         }
 
+        /// <summary>Override of the <c>Write</c> method, writes to each <c>TextWriter</c> in the list of <c>TextWriter</c>s</summary>
+        /// <param name="value"><c>string</c> to be written</param>
+        /// <returns><c>void</c></returns>
+        /// <author>Damian Jimenez</author>
         public override void Write(string value)
         {
             foreach (var writer in writers)
                 writer.Write(value);
         }
 
+        /// <summary>Override of the <c>Flush</c> method, flushes each <c>TextWriter</c> in the list of <c>TextWriter</c>s</summary>
+        /// <returns><c>void</c></returns>
+        /// <author>Damian Jimenez</author>
         public override void Flush()
         {
             foreach (var writer in writers)
                 writer.Flush();
         }
 
+        /// <summary>Override of the <c>Close</c> method, closes each <c>TextWriter</c> in the list of <c>TextWriter</c>s</summary>
+        /// <returns><c>void</c></returns>
+        /// <author>Damian Jimenez</author>
         public override void Close()
         {
             foreach (var writer in writers)
                 writer.Close();
         }
 
+        /// <summary>Override of the <c>Encoding</c> method, returns the <c>Encoding</c> of the <c>TextWriter</c>s</summary>
+        /// <returns><c>Encoding.ASCII</c></returns>
+        /// <author>Damian Jimenez</author>
         public override Encoding Encoding
         {
             get { return Encoding.ASCII; }
@@ -330,7 +424,7 @@ namespace CustomACEAPI
 
         /// <summary>Method that allows a user to check whether the connection to the ACE server has been started</summary>
         /// <author>Damian Jimenez</author>
-        /// <returns>bool specifying whether the connection to the ACE server is active or not</returns>
+        /// <returns><c>bool</c> specifying whether the connection to the ACE server is active or not</returns>
         public bool Connected
         {
             get
@@ -341,7 +435,7 @@ namespace CustomACEAPI
 
         /// <summary>Method that returns the class instance of IAceServer</summary>
         /// <author>Damian Jimenez</author>
-        /// <returns>IAceServer the ace server used to communicate with ACE and the robot</returns>
+        /// <returns><c>IAceServer</c> the ace server used to communicate with ACE and the robot</returns>
         public IAceServer AceServer
         {
             get
@@ -352,7 +446,7 @@ namespace CustomACEAPI
 
         /// <summary>Method that returns the class instance of IAdeptRobot</summary>
         /// <author>Damian Jimenez</author>
-        /// <returns>IAdeptRobot the robot that is to be controlled</returns>
+        /// <returns><c>IAdeptRobot</c> the robot that is to be controlled</returns>
         public IAdeptRobot AceRobot
         {
             get
@@ -365,10 +459,10 @@ namespace CustomACEAPI
         /// <author>Damian Jimenez</author>
         /// <param name="controllerPath">Specifies where the server should search for the controller, can be found by opening up a default workspace in Adept ACE and connecting to a desired robot.</param>
         /// <param name="robotPath">Specifies where the server should search for the robot, can be found by opening up a default workspace in Adept ACE and connecting to a desired robot.</param>
-        /// <returns>Nothing</returns>
+        /// <returns><c>void</c></returns>
         public void ConnectToServer(String controllerPath = "/SmartController 9/SmartController 9", String robotPath = "/SmartController 9/R1 Viper650")
         {
-
+            // Set up the server to allow remote connections and connect to the ACE server
             RemotingUtil.InitializeRemotingSubsystem(true, CALLBACK_PORT);
             ace = (IAceServer)RemotingUtil.GetRemoteServerObject(typeof(IAceServer), REMOTING_NAME, REMOTING_HOST, REMOTING_PORT);
 
@@ -379,6 +473,7 @@ namespace CustomACEAPI
                 Console.WriteLine($"Found controller: {controller.FullPath}");
                 if(controllerPath.Equals(controller.FullPath))
                 {
+                    // Set up the controller
                     controller.Enabled = true;
                     controller.HighPower = true;
                     controller.Calibrate();
@@ -387,6 +482,8 @@ namespace CustomACEAPI
                         controller.DryRun = true;
                         controller.DryRun = false;
                     }
+
+                    // Add any robots that are found associated with that controller
                     AdeptControllerUtil.AddAllRobots(ace, controller, ace.Root);
 
                     // Print out all the robots that are found connected to the workspace and available
