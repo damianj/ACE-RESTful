00fd7c2a56d48e2cf7bf64ea982912e529051874
Mars Lab <marslab.cse@gmail.com>
2017-04-27 14:40:17 -0500

    Fixed some issues with the code
    Resolved the issue of being unable to write to the textbox, had to set up a dispatcher to re-route the call to the thread the GUI was running on. Made some other changes to make more info be output the GUI so the user knows what is going on.

--- CustomACEApi/MainWindow.xaml.cs
+++ CustomACEApi/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using Nancy.Hosting.Self;
 using Newtonsoft.Json;
 using System.Threading;
 using System.IO;
-
+using System.Windows.Threading;
 
 namespace CustomACEAPI
 {
@@ -36,12 +36,14 @@ namespace CustomACEAPI
         private const int _port = 9001;
         private static NancyHost _nancy;
         private static AdeptAce adept_ace;
+        private static Dispatcher gui_dispatcher;
 
         /// <summary>Initiates the GUI and preps everything needed to successfully start the NancyFX server and connect to the Adept ACE server </summary>
         /// <author>Damian Jimenez</author>
         /// <returns><c>void</c></returns>
         public MainWindow()
         {
+            gui_dispatcher = Dispatcher;
             adept_ace = new AdeptAce();
             InitializeComponent();
             OutputText.Text = "";
@@ -58,7 +60,7 @@ namespace CustomACEAPI
 
             // Prep the NancyFX server for use later on
             _nancy = new NancyHost(configuration, new Uri($"{_url}:{_port}/"));
-            Console.WriteLine("App started successfully...\n---------------------------");
+            WriteOutput("App started successfully...\n---------------------------");
         }
 
         /// <summary>Starts the NancyFX server to allow it to start listening for requests on localhost:9001</summary>
@@ -76,7 +78,7 @@ namespace CustomACEAPI
             
             // Start the HTTP server on localhost:9001
             _nancy.Start();
-            Console.WriteLine($"Listening on {_url}:{_port}/\n");
+            WriteOutput($"Listening on: {_url}:{_port}/\n");
         }
 
         /// <summary>Stops the NancyFX server</summary>
@@ -88,9 +90,20 @@ namespace CustomACEAPI
         {
             // Stop the HTTP Server
             _nancy.Stop();
-            Console.WriteLine("Successfully stopped the HTTP server.\n------------------------------------\n");
+            WriteOutput("Successfully stopped the HTTP server.\n------------------------------------\n");
         }
 
+        /// <summary>Writes to the console and the GUI text-box. Necessary to re-route calls from different threads onto the thread the GUI is running on.</summary>
+        /// <author>Damian Jimenez</author>
+        /// <param name="output_text">String to be written to the console and GUI text-box</param>
+        /// <returns><c>void</c></returns>
+        public static void WriteOutput(String output_text)
+        {
+            gui_dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+            {
+                Console.WriteLine(output_text);
+            }));
+        }
 
         /// <summary>Class to handle CartesianMove API calls</summary>
         /// <author>Damian Jimenez</author>
@@ -101,8 +114,6 @@ namespace CustomACEAPI
             /// <returns><c>void</c></returns>
             public CartesianMoveAPI()
             {
-                /// <author>Damian Jimenez</author>
-                /// <returns><c>Response</c> object specifying to the user that GET requests are not supported</returns>
                 Get["/api/move/cartesian"] = _ =>
                 {
                     return new Response()
@@ -119,8 +130,6 @@ namespace CustomACEAPI
                     };
                 };
 
-                /// <author>Damian Jimenez</author>
-                /// <returns><c>HttpStatusCode.OK</c> or a Response object detailing what went wrong</returns>
                 Post["/api/move/cartesian"] = _ =>
                 {
                     try
@@ -133,15 +142,14 @@ namespace CustomACEAPI
                         var body = Encoding.Default.GetString(data);
                         var command = JsonConvert.DeserializeObject<CartesianMoveCommand>(body);
 
-                        Console.WriteLine($"Received the following POST request:\n{body.ToString()}\n");
-                        Thread command_thread = new Thread(() => command.Execute(adept_ace.AceRobot, adept_ace.AceServer.CreateObject(typeof(CartesianMove)) as CartesianMove));
-                        command_thread.Start();
+                        WriteOutput($"Received the following POST request:\n{body.ToString()}\n");
+                        command.Execute(adept_ace.AceRobot, adept_ace.AceServer.CreateObject(typeof(CartesianMove)) as CartesianMove);
 
                         return HttpStatusCode.OK;
                     }
                     catch (Exception e)
                     {
-                        Console.WriteLine($"ERROR:\n{e.Message}\n");
+                        WriteOutput($"ERROR:\n{e.Message}\n");
                         string jsonString = $"{{ status: \"failure\", error: \"{e.Message}\" }}";
                         byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonString);
 
@@ -214,59 +222,47 @@ namespace CustomACEAPI
             /// <returns><c>void</c></returns>
             public void Execute(IAdeptRobot robot, CartesianMove cartesianMove)
             {
-                _SEMAPHORE.WaitOne();
-                /**********************************************************************
-                *** Protected block prevents this command from firing con-currently ***
-                ********* and sending conflicting instructions to the robot. **********
-                **********************************************************************/
+                // Get the current joint positions of the robot
+                double[] jointPositions = robot.JointPosition;
+
+                // Print out joint information for the user to see
+                foreach(var joint in jointPositions.Select((value, i) => new { i, value }))
                 {
-                    // Get the current joint positions of the robot
-                    double[] jointPositions = robot.JointPosition;
-
-                    // Print out joint information for the user to see
-                    foreach(var joint in jointPositions.Select((value, i) => new { i, value }))
-                    {
-                        Console.WriteLine($"joint[{joint.i}] = {joint.value}");
-                    }
-
-                    // Assign the cartesianMove the parameters that were passed via the API call
-                    cartesianMove.Param.Accel = Accel;
-                    cartesianMove.Param.Decel = Decel;
-                    cartesianMove.Param.Speed = Speed;
-                    cartesianMove.Param.Straight = StraightMotion;
-                    cartesianMove.Param.MotionEnd = MOTION_END[MotionEnd];
-                    cartesianMove.Param.SCurveProfile = SCurveProfile;
-
-                    // Transform the current joint position to a world location
-                    Transform3D loc = robot.JointToWorld(jointPositions);
-
-                    // Check if the current location is inrange           
-                    Transform3D currentPosition = robot.WorldLocationWithTool;
-                    int inRange = robot.InRange(currentPosition);
-                    Console.WriteLine(currentPosition + " inrange check = " + inRange);
-
-                    // Get the current robot configuration
-                    IMoveConfiguration moveConfig = robot.GetMoveConfiguration(jointPositions);
-
-                    // Create a new Transform3D object specifying how the robot should move
-                    cartesianMove.MoveConfiguration = moveConfig;
-                    Transform3D transform = new Transform3D(X, Y, Z, Yaw, Pitch, Roll);
-
-                    // Calculate the new position of the robot based on the transform and the robots current position
-                    cartesianMove.WorldLocation = currentPosition * transform;
-
-                    // Issue the move and wait until it is done
-                    robot.Move(cartesianMove);
-                    robot.WaitMoveDone();
-
-                    // Force the robot to issue a DETACH
-                    robot.AutomaticControlActive = false;
+                    WriteOutput($"joint[{joint.i}] = {joint.value}");
                 }
-                /*********************************************************************
-                *********************** End of protected block ***********************
-                **********************************************************************
-                *********************************************************************/
-                _SEMAPHORE.Release();
+
+                // Assign the cartesianMove the parameters that were passed via the API call
+                cartesianMove.Param.Accel = Accel;
+                cartesianMove.Param.Decel = Decel;
+                cartesianMove.Param.Speed = Speed;
+                cartesianMove.Param.Straight = StraightMotion;
+                cartesianMove.Param.MotionEnd = MOTION_END[MotionEnd];
+                cartesianMove.Param.SCurveProfile = SCurveProfile;
+
+                // Transform the current joint position to a world location
+                Transform3D loc = robot.JointToWorld(jointPositions);
+
+                // Check if the current location is inrange           
+                Transform3D currentPosition = robot.WorldLocationWithTool;
+                int inRange = robot.InRange(currentPosition);
+                WriteOutput($"[{currentPosition}], inrange check = {inRange}\n");
+
+                // Get the current robot configuration
+                IMoveConfiguration moveConfig = robot.GetMoveConfiguration(jointPositions);
+
+                // Create a new Transform3D object specifying how the robot should move
+                cartesianMove.MoveConfiguration = moveConfig;
+                Transform3D transform = new Transform3D(X, Y, Z, Yaw, Pitch, Roll);
+
+                // Calculate the new position of the robot based on the transform and the robots current position
+                cartesianMove.WorldLocation = currentPosition * transform;
+
+                // Issue the move and wait until it is done
+                robot.Move(cartesianMove);
+                robot.WaitMoveDone();
+
+                // Force the robot to issue a DETACH
+                robot.AutomaticControlActive = false;
             }
         }
     }
@@ -459,12 +455,14 @@ namespace CustomACEAPI
                     AdeptControllerUtil.AddAllRobots(ace, controller, ace.Root);
 
                     // Print out all the robots that are found connected to the workspace and available
-                    foreach (IAdeptController robot in ace.Root.Filter(new ObjectTypeFilter(typeof(IAdeptRobot)), true))
+                    foreach (IAdeptRobot available_robot in ace.Root.Filter(new ObjectTypeFilter(typeof(IAdeptRobot)), true))
                     {
-                        Console.WriteLine($"Found robot: {robot}");
+                        Console.WriteLine($"Found robot: {available_robot.FullPath}");
+                        if(robotPath.Equals(available_robot.FullPath))
+                        {
+                            robot = available_robot;
+                        }
                     }
-
-                    robot = ace.Root[robotPath] as IAdeptRobot;
                 }
             }
 
